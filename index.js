d3769f9df127103672a5a5982ed49bbbd174f9ac
a-shevelev <60062720+a-shevelev@users.noreply.github.com>
2020-02-16 14:24:58 +0300

    complete

---
+++ index.js
@@ -0,0 +1,18 @@
+let mc = require("mongodb").MongoClient;
+let url = "mongodb://localhost";
+
+mc.connect(url,
+function(err, client){
+    if(err) console.log(err.stack);
+    var human = {name: "Swyatogor", age: 132};
+    let db = client.db('sandbox');
+    db.collection('humans').insertOne(human,
+    	function(error,response){
+    		if (error)
+    		console.log("Error insert"); 
+    	else
+    		console.log("inserted record", response.ops[0]);
+
+    	});
+    client.close();
+});
